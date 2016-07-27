60af7dfcdc49449357454fa9aa3f3a02f9b8d79a
Deli <ibnu@bosnetdis.com>
2016-07-27 17:39:12 +0700

    save status: - table manager , get schema - getSchemaTest in table manager

--- SQLMigrationInterface/SQLMigrationInterface/Data/ConfigData.cs
+++ SQLMigrationInterface/SQLMigrationInterface/Data/ConfigData.cs
@@ -15,14 +15,14 @@ namespace SQLMigration.Interface.Data
         {
             OutputPath = "";
             updated =  DateTime.Now;
-            id = Guid.NewGuid().ToString();  
-            //listUDTSchemaInfo = new List<UDTSchemaInfoData>();
-            //listUDTResultInfo = new List<UDTResultData>();
-            //listTableSchemaInfo = new List<TableSchemaInfoData>();
-            //listTableResultInfo = new List<TableResultData>();
+            id = Guid.NewGuid().ToString();
+            listUDTSchemaInfo = new List<UDTSchemaInfoData>();
+            listUDTResultInfo = new List<UDTResultData>();
+            listTableSchemaInfo = new List<TableSchemaInfoData>();
+            listTableResultInfo = new List<TableResultData>();
 
-            allSchemaInfo = new AllSchemaInfoData();
-            allResultInfo = new AllResultData(); 
+            //allSchemaInfo = new AllSchemaInfoData();
+            //allResultInfo = new AllResultData(); 
             
             name = "";
 
@@ -32,20 +32,20 @@ namespace SQLMigration.Interface.Data
         public string OutputPath { get; set; }
         public DBData Source { get; set; }
 
-        //public List<UDTSchemaInfoData> listUDTSchemaInfo { get; set; }
-        //public List<UDTResultData> listUDTResultInfo { get; set; }
-        //public List<TableSchemaInfoData> listTableSchemaInfo { get; set; }
-        //public List<TableResultData> listTableResultInfo { get; set; }
-        public AllSchemaInfoData allSchemaInfo { get; set; }
-        public AllResultData allResultInfo { get; set; }
+        public List<UDTSchemaInfoData> listUDTSchemaInfo { get; set; }
+        public List<UDTResultData> listUDTResultInfo { get; set; }
+        public List<TableSchemaInfoData> listTableSchemaInfo { get; set; }
+        public List<TableResultData> listTableResultInfo { get; set; }
+        //public AllSchemaInfoData allSchemaInfo { get; set; }
+        //public AllResultData allResultInfo { get; set; }
 
         public override string ToString()
         {
             return string.Format("ConfigData => {0} OutputPath: {6} , \r\n " +
                                  "Destination: {1}, Source: {7}, \r\n" +
-                                 "allResultInfo.listUDTResultInfo: {2}, allResultInfo.listTableResultInfo: {3}" +
-                                 "allSchemaInfo.listUDTSchemaInfo: {4}, allSchemaInfo.listTableSchemaInfo: {5}", 
-                base.ToString(), Destination, allResultInfo.listUDTResultInfo.Count,allResultInfo.listTableResultInfo.Count, allSchemaInfo.listUDTSchemaInfo.Count, allSchemaInfo.listTableSchemaInfo.Count, OutputPath, Source);
+                                 "listUDTResultInfo: {2}, listTableResultInfo: {3}" +
+                                 "listUDTSchemaInfo: {4}, listTableSchemaInfo: {5}", 
+                base.ToString(), Destination, listUDTResultInfo.Count,listTableResultInfo.Count, listUDTSchemaInfo.Count, listTableSchemaInfo.Count, OutputPath, Source);
         }
     }
 }
---
+++ SQLMigrationTest/TableManagerTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EasyTools.Interface.DB;
+using SQLMigrationInterface.Interface.ScriptBuilder;
+using SQLMigrationInterface.Interface.SourceQuery;
+using FakeItEasy;
+using System.Data;
+using SQLMigration.Interface.Data;
+using SQLMigration.Interface.Interface.Manager;
+using SQLMigrationManager;
+using SQLMigration.Data.SchemaInfo;
+
+namespace SQLMigration.Test
+{
+    [TestClass]
+    public class TableManagerTest
+    {
+        [TestMethod]
+        public void GetSchemaTes()
+        {
+            var dataAccess = A.Fake<IDataAccess>();
+            var scriptBuilder = A.Fake<IScriptBuilder>();
+            var schemaQuery = A.Fake<ISourceQuery>();
+
+            DataTable resultDataAccess = new DataTable("DataTable");         
+
+            resultDataAccess.Columns.Add("TABLE_NAME");
+            resultDataAccess.Columns.Add("COLUMN_NAME");
+            resultDataAccess.Columns.Add("ORDINAL_POSITION");
+            resultDataAccess.Columns.Add("COLUMN_DEFAULT");
+            resultDataAccess.Columns.Add("IS_NULLABLE");
+            resultDataAccess.Columns.Add("DOMAIN_NAME");
+            resultDataAccess.Columns.Add("DATA_TYPE");
+            resultDataAccess.Columns.Add("CHARACTER_MAXIMUM_LENGTH");
+            resultDataAccess.Columns.Add("NUMERIC_PRECISION");
+            resultDataAccess.Columns.Add("NUMERIC_SCALE");
+
+            DataRow dataRow = resultDataAccess.NewRow();
+           
+            dataRow["TABLE_NAME"] = "customTableName";
+            dataRow["COLUMN_NAME"] = "customColumnName";
+            dataRow["ORDINAL_POSITION"] = "1";
+            dataRow["COLUMN_DEFAULT"] = "";
+            dataRow["IS_NULLABLE"] = "1";
+            dataRow["DOMAIN_NAME"] = "customDomain";
+            dataRow["DATA_TYPE"] = "numeric";
+            dataRow["CHARACTER_MAXIMUM_LENGTH"] = "0";
+            dataRow["NUMERIC_PRECISION"] = "8";
+            dataRow["NUMERIC_SCALE"] = "2";
+
+            resultDataAccess.Rows.Add(dataRow);
+
+
+            var configData = new ConfigData { name = "Config1" };
+
+            var resultQuery = "";
+
+            A.CallTo(() => schemaQuery.GetTableQuery()).Returns(resultQuery);
+            A.CallTo(() => dataAccess.GetDataTable(configData.Source, resultQuery)).Returns(resultDataAccess);
+
+            ITableManager udtManager = new TableManager(dataAccess, scriptBuilder, schemaQuery);
+
+            var listSchema = udtManager.GetSchema(configData);
+
+            var schemaExpectation = new TableSchemaInfoData
+            {
+                TableName = "customTableName",
+                ColumnName = "customColumnName",
+                OrdinalPosition = 1,
+                ColumnDefault = "",
+                isNullable = true,
+                Domain = "customDomain",
+                DataType = "numeric",
+                CharMaxLength = 0,
+                Precision = 8,
+                Scale = 2,
+        };
+
+            var schemaActual = listSchema[0];
+
+            Assert.AreEqual(schemaExpectation.TableName, schemaActual.TableName);
+            Assert.AreEqual(schemaExpectation.ColumnName, schemaActual.ColumnName);
+            Assert.AreEqual(schemaExpectation.OrdinalPosition, schemaActual.OrdinalPosition);
+            Assert.AreEqual(schemaExpectation.ColumnDefault, schemaActual.ColumnDefault);
+            Assert.AreEqual(schemaExpectation.isNullable, schemaActual.isNullable);
+            Assert.AreEqual(schemaExpectation.Domain, schemaActual.Domain);
+            Assert.AreEqual(schemaExpectation.DataType, schemaActual.DataType);
+            Assert.AreEqual(schemaExpectation.CharMaxLength, schemaActual.CharMaxLength);
+            Assert.AreEqual(schemaExpectation.Precision, schemaActual.Precision);
+            Assert.AreEqual(schemaExpectation.Scale, schemaActual.Scale);          
+
+        }
+    }
+}
