2bce638a37355fe987cf7d02c48bb7e7528e869e
Deli <ibnu@bosnetdis.com>
2016-07-27 15:31:58 +0700

    save ulang

---
+++ SQLMigrationInterface/SQLMigrationInterface/Data/ResultInfo/TableResultData.cs
@@ -0,0 +1,25 @@
+﻿using EasyTools.Data;
+using System;
+
+namespace SQLMigration.Data.ResultInfo
+{
+    public class TableResultData : BaseData
+    {
+        public string schemaId { get; set; }
+        public string sqlString { get; set; }
+
+        public TableResultData()
+        {
+            id = Guid.NewGuid().ToString();
+            name = "";
+            sqlString = "";
+            schemaId = "";
+            updated = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("TableResultData => {0}, schemaId: {1}, sqlString: {2}", base.ToString(), schemaId, sqlString);
+        }
+    }
+}
